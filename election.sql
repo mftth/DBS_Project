cc240e634af215724bbbbddb75349cbc2f2f3596
thbake <thomas.bake@hotmail.com>
2017-05-30 12:30:26 +0200

    Update election.sql
    CREATE TABLE Statements

--- election.sql
+++ election.sql
@@ -1,8 +1,6 @@
-createdb election
-psql -d election -U postgres
 CREATE TABLE "User"(screenname CHAR(30));
 CREATE TABLE "Hashtag"(hashtagname CHAR(30));
-CREATE TABLE "Tweet"(tweettext CHAR(140), tweetid INT, favouritecount INT, retweetcount INT, originalauthor CHAR(20), truncated BOOLEAN, isquotestatus BOOLEAN, sourceurl CHAR(100), isretweet BOOLEAN, inreplytoscreenname BOOLEAN);
+CREATE TABLE "Tweet"(tweettext CHAR(140), tweetid INT, favouritecount INT, retweetcount INT, originalauthor CHAR(20), truncated BOOLEAN, isquotestatus BOOLEAN, sourceurl CHAR(100), isretweet BOOLEAN, inreplytoscreenname CHAR(20));
 alter table "User" add primary key (screenname);
 alter table "Tweet" add primary key (tweetid);
 alter table "Hashtag" add primary key (hashtagname);
