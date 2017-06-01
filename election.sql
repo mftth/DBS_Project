7d560c7d78937f19aa83bcce62274cea91c18041
thbake <thomas.bake@hotmail.com>
2017-06-02 00:53:51 +0200

    Update election.sql

--- election.sql
+++ election.sql
@@ -1,6 +1,6 @@
 create table "ENTHAELT"(tweet_id INT, hashtag_name CHAR(30));
 create table "HASHTAG"(hashtag_name CHAR(30));
-create table "POSTET"(tweet_id INT, screen_name CHAR(30), time CHAR(25));
+create table"POSTET"(tweet_id INT, screen_name CHAR(30), time CHAR(25));
 create table "TWEET"(tweet_id INT, tweet_text CHAR(300), original_author CHAR(20), favourite_count INT, in_reply_to_screen_name CHAR(20), is_quote_status BOOLEAN, is_retweet BOOLEAN, retweet_count INT, truncated BOOLEAN, source_url CHAR(100));
 create table "USER"(screen_name CHAR(30));
 alter table "USER" add primary key (screen_name);
