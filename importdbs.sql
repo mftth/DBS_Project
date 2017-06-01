26cb6414438e713bda330aaf156c2f1f9d3c60ac
thbake <thomas.bake@hotmail.com>
2017-06-02 00:11:05 +0200

    Add files via upload

---
+++ importdbs.sql
@@ -0,0 +1,9 @@
+
+
+COPY "public"."ENTHAELT" FROM '/tmp/enthaelt.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
+COPY "public"."HASHTAG" FROM '/tmp/hashtags.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
+COPY "public"."POSTET" FROM '/tmp/postet.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
+COPY "public"."TWEET" FROM '/tmp/tweet.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
+COPY "public"."USER" FROM '/tmp/us.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
+
+
