0502cd5d431474202aa89f939b4748cb4bbb391b
thbake <thomas.bake@hotmail.com>
2017-06-02 00:55:39 +0200

    Update importdbs.sql

--- importdbs.sql
+++ importdbs.sql
@@ -4,4 +4,3 @@ COPY "public"."POSTET" FROM '/tmp/postet.csv' DELIMITER ';' CSV ENCODING 'WIN125
 COPY "public"."TWEET" FROM '/tmp/tweet.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
 COPY "public"."USER" FROM '/tmp/user.csv' DELIMITER ';' CSV ENCODING 'WIN1252';
 
-
